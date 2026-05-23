1be6c1619b078b20c0edbaec37e4dc31db053051
Michael Amponsah <michaelamponsah155@gmail.com>
2026-05-23 09:23:41 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Dashboard from './Dashboard';
+import React from 'react'
+import Dashboard from './Dashboard.jsx'
 
 export default function App() {
   return (
-    <div className="w-full h-screen bg-gray-50 m-0 p-0 overflow-x-hidden">
+    <div class="w-full min-h-screen bg-gray-50 m-0 p-0 overflow-x-hidden">
       <Dashboard />
     </div>
-  );
+  )
 }
