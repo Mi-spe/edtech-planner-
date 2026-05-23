bab563a3924459350d81496e3814fe8f1d6c3881
Michael Amponsah <michaelamponsah155@gmail.com>
2026-05-23 07:32:18 +0000

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-gray-50 m-0 p-0 overflow-x-hidden">
+      <Dashboard />
+    </div>
+  );
+}
